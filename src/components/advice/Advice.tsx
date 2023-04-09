4d7c529d4b46f4f5f0190127b2cf874b61a99561
Appz-dk <patrick.fyhn@gmail.com>
2023-04-09 22:22:39 +0200

    Setup advice fetching logic + props to children

--- src/components/advice/Advice.tsx
+++ src/components/advice/Advice.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import Card from "../Card";
 import AdviceBtn from "./AdviceBtn";
 import AdviceText from "./AdviceText";
 import Divider from "./Divider";
+import { TAdvice, getRandomAdvice } from "../../api/getRandomAdvice";
+
+const DEFAULT_ADVICE = {
+  id: "-1",
+  text: "Technology doesn't always work as intended...",
+};
 
 const Advice = () => {
+  const [advice, setAdvice] = useState<TAdvice>(DEFAULT_ADVICE);
+
+  const setNewAdvice = async () => {
+    setAdvice(await getRandomAdvice());
+  };
+
+  // Fetching first advice on page render
+  useEffect(() => {
+    // IIFE: Immediately Invoked Function Expression
+    (async () => {
+      setAdvice(await getRandomAdvice());
+    })();
+  }, []);
+
   return (
     <Card>
-      <AdviceText />
+      <AdviceText advice={advice} />
       <Divider />
-      <AdviceBtn />
+      <AdviceBtn getNewAdvice={setNewAdvice} />
     </Card>
   );
 };

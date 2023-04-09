8817531079260c79f3847875a27cc51bd30166e9
Appz-dk <patrick.fyhn@gmail.com>
2023-04-09 22:02:31 +0200

    Component structure done

---
+++ src/components/advice/Advice.tsx
@@ -0,0 +1,16 @@
+import Card from "../Card";
+import AdviceBtn from "./AdviceBtn";
+import AdviceText from "./AdviceText";
+import Divider from "./Divider";
+
+const Advice = () => {
+  return (
+    <Card>
+      <AdviceText />
+      <Divider />
+      <AdviceBtn />
+    </Card>
+  );
+};
+
+export default Advice;

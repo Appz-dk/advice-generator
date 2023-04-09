21f21f4ec89e166f9c723c5e76a9971bd7a429b6
Appz-dk <patrick.fyhn@gmail.com>
2023-04-09 22:20:34 +0200

    Implemented advice data from props

--- src/components/advice/AdviceText.tsx
+++ src/components/advice/AdviceText.tsx
@@ -1,13 +1,16 @@
+import { TAdvice } from "../../api/getRandomAdvice";
 import classes from "./AdviceText.module.css";
 
-const AdviceText = () => {
+type AdviceTextProps = {
+  advice: TAdvice;
+};
+
+const AdviceText: React.FC<AdviceTextProps> = ({ advice }) => {
   return (
     <div className={classes["container"]}>
       {/* TODO: Edit h1 tag to something else if used on page already with an h1 */}
-      <h1 className={classes.title}>Advice #117</h1>
-      <q className={classes.quote}>
-        It is easy to situp and take notice, what's difficult is getting up and taking action
-      </q>
+      <h1 className={classes.title}>Advice #{advice.id}</h1>
+      <q className={classes.quote}>{advice.text}</q>
     </div>
   );
 };

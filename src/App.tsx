d752738c6c0e64d91deabbc989cab37e5eac03aa
Appz-dk <patrick.fyhn@gmail.com>
2023-04-09 22:39:27 +0200

    Added main landmark to page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Advice from "./components/advice/Advice";
 
 function App() {
   return (
-    <div className="app">
+    <main className="app">
       <Advice />
-    </div>
+    </main>
   );
 }
 
